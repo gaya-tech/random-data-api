521d74ad3fe92526ea69c47f31973f8aa1808810
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-08-12 00:37:43 +0530

    Fixes the cors issue

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const {dataMaker} = require('./makeRandomData')
+const cors = require('cors');
 const app = express()
 
+app.use(cors())
 
-app.get("/api/v1/random-data/:number", (req, res) => {
+app.get("/api/v1/products/:number", (req, res) => {
     let data= dataMaker( req.params.number)
     return res.json({
         number: req.params.number,
         data: data
     })
+})
+
+app.get('*', (req,res) => {
+    res.send({data: "Bad Request"})
 })
 
 const PORT = process.env.PORT || 3000
